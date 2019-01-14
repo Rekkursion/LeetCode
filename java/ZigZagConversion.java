2301e3563dc36febef01955c2697c83ea188e85d
Rekkursion <35969022+Rekkursion@users.noreply.github.com>
2019-01-14 12:57:36 +0800

    Create ZigZagConversion.java

---
+++ java/ZigZagConversion.java
@@ -0,0 +1,34 @@
+// 6. ZigZag Conversion
+// Accepted 63ms
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1)
+            return s;
+        
+        int modulo = (numRows * 2) - 2;
+        int remainder;
+        String[] rets = new String[numRows];
+        String ret;
+        
+        for(int k = 0; k < numRows; k++)
+            rets[k] = "";
+        
+        for(int k = 0; k < s.length(); ++k) {
+            remainder = k % modulo;
+            
+            if(remainder < numRows) {
+                rets[remainder] += s.charAt(k);
+            }
+            else {
+                rets[modulo - remainder] += s.charAt(k);
+            }
+        }
+        
+        ret = "";
+        for(int k = 0; k < numRows; k++)
+            ret += rets[k];
+        
+        return ret;
+    }
+}
