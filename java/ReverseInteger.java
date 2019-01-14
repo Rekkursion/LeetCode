986421f46f5161ac67fede634e379625e080932a
Rekkursion <35969022+Rekkursion@users.noreply.github.com>
2019-01-14 13:17:06 +0800

    Update ReverseInteger.java

--- java/ReverseInteger.java
+++ java/ReverseInteger.java
@@ -1,28 +1,23 @@
 // 7. Reverse Integer
-// Accepted 42ms
+// Accepted 28ms
 
 class Solution {
     public int reverse(int x) {
         long rx = 0L;
-        ArrayList<Integer> seq = new ArrayList<Integer>();
         boolean isNegative = (x < 0);
         
         if(isNegative)
             x *= -1;
         
         while(x > 0) {
-            seq.add(x % 10);
+            rx = (rx * 10L) + (x % 10);
             x /= 10;
-        }
-        
-        for(Integer i: seq) {
-            rx = (rx * 10L) + (long)i;
         }
         
         if(isNegative)
             rx *= -1L;
         
-        if(rx < (long)Integer.MIN_VALUE || rx > (long)Integer.MAX_VALUE)
+        if(rx < Integer.MIN_VALUE || rx > Integer.MAX_VALUE)
             rx = 0L;
         
         return (int)rx;
