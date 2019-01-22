8eb67e141080529ea46b4e9043a040457b30ce4f
Rekkursion <35969022+Rekkursion@users.noreply.github.com>
2019-01-22 15:52:05 +0800

    Create 064. Minimum Path Sum.java

---
+++ java/064. Minimum Path Sum.java
@@ -0,0 +1,24 @@
+// 64. Minimum Path Sum
+// Accepted 4ms
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+        
+        int[][] path = new int[grid.length][grid[0].length];
+        
+        path[0][0] = grid[0][0];
+        for(int i = 1; i < grid.length; ++i)
+            path[i][0] = path[i - 1][0] + grid[i][0];
+        for(int j = 1; j < grid[0].length; ++j)
+            path[0][j] = path[0][j - 1] + grid[0][j];
+        
+        for(int i = 1; i < grid.length; ++i) {
+            for(int j = 1; j < grid[i].length; ++j)
+                path[i][j] = Math.min(path[i - 1][j], path[i][j - 1]) + grid[i][j];
+        }
+        
+        return path[grid.length - 1][grid[0].length - 1];
+    }
+}
