1eb76cd4f0b9c6c5a505692c9cd771cc2c244ed9
Rekkursion <35969022+Rekkursion@users.noreply.github.com>
2019-01-24 10:26:53 +0800

    Update 085. Maximal Rectangle.java

--- java/085. Maximal Rectangle.java
+++ java/085. Maximal Rectangle.java
@@ -1,5 +1,5 @@
 // 85. Maximal Rectangle
-// Accepted 33ms
+// Accepted 32ms
 
 class Solution {
     public int maximalRectangle(char[][] matrix) {
@@ -7,17 +7,14 @@ class Solution {
             return 0;
         
         int[][] rowAcc = new int[matrix.length][matrix[0].length];
-        int[][] acc = new int[matrix.length][matrix[0].length];
         int curMax, curLen, finalMax;
         
         finalMax = 0;
         for(int k = 0; k < matrix.length; ++k) {
             for(int j = 0; j < matrix[k].length; ++j) {
                 rowAcc[k][j] = matrix[k][j] == '0' ? 0 : (j == 0 ? 0 : rowAcc[k][j - 1]) + 1;
-                if(k == 0) {
-                    acc[k][j] = rowAcc[k][j];
-                    finalMax = Math.max(finalMax, acc[k][j]);
-                }
+                if(k == 0)
+                    finalMax = Math.max(finalMax, rowAcc[k][j]);
             }
         }
         
