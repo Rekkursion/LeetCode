432a80e9c450e7d778f7a89a9530eb5f113bd087
Rekkursion <35969022+Rekkursion@users.noreply.github.com>
2019-01-25 10:55:32 +0800

    Create 001. Two Sum.py

---
+++ python3/001. Two Sum.py
@@ -0,0 +1,23 @@
+# 1. Two Sum
+# 4228ms
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i, vi in enumerate(nums):
+            for j, vj in enumerate(nums[i + 1:]):
+                if vi + vj == target:
+                    ret = [i, j + i + 1]
+                    break
+            else:
+                continue
+            break
+        else:
+            ret = [0, 0]
+        
+        return ret
+        
