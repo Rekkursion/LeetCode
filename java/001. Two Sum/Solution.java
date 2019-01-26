eb0bcd3ca0b9efab12b1af65e23a29297579bbd9
Rekkursion <35969022+Rekkursion@users.noreply.github.com>
2019-01-26 10:00:24 +0800

    change loc

--- java/001. Two Sum/Solution.java
+++ java/001. Two Sum/Solution.java
@@ -4,7 +4,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] retIdcs = new int[2];
-        
         for(int k = 0; k < nums.length; k++) {
             for(int j = k + 1; j < nums.length; j++) {
                 if(nums[k] + nums[j] == target) {
