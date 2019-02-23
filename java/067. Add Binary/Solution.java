3ef44d747eaf35a8b3ed40c3aeb44cedd0888e2a
Rekkursion <35969022+Rekkursion@users.noreply.github.com>
2019-02-23 16:59:48 +0800

    Create Solution.java

---
+++ java/067. Add Binary/Solution.java
@@ -0,0 +1,20 @@
+// 67. Add Binary
+// Accepted 2ms
+
+class Solution {
+    public String addBinary(String a, String b) {
+        int lenA = a.length();
+        int lenB = b.length();
+        int carry = 0, added;
+        StringBuilder sBuf = new StringBuilder();
+        
+        for(int k = lenA - 1, j = lenB - 1; k >= 0 || j >= 0 || carry != 0; --k, --j) {
+            int ia = (k < 0) ? 0 : (a.charAt(k) - 48);
+            int ib = (j < 0) ? 0 : (b.charAt(j) - 48);
+            sBuf.append(ia ^ ib ^ carry);
+            carry = ((ia & ib) | (ia & carry) | (ib & carry));
+        }
+        
+        return sBuf.reverse().toString();
+    }
+}
